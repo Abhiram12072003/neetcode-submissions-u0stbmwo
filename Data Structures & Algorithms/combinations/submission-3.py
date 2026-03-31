f97740de0fb639df66cf939105ea8ab3ad2419e7
Abhiram12072003 <94527813+Abhiram12072003@users.noreply.github.com>
2026-03-31 16:14:12 +0530

    Add: combinations - submission-3

---
+++ Data Structures & Algorithms/combinations/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        xi = []
+        ans = []
+        def func(itr, xi):
+            if len(xi) == k:
+                ans.append(xi[:])
+                return
+            for i in range(itr, n+1):
+                xi.append(i)
+                func(i+1, xi)
+                xi.pop()
+            return
+        func(1,xi)
+        return ans
