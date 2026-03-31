a8ec27501600a2f5a02fdca57030fa4b60e425e4
Abhiram12072003 <94527813+Abhiram12072003@users.noreply.github.com>
2026-03-31 16:44:14 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = []
+        
+        def func(itr, xi):
+            if itr == len(nums):
+                ans.append(xi[:])
+                return
+
+            for i in range(itr, n):
+                nums[itr], nums[i] = nums[i], nums[itr]
+                xi.append(nums[itr])
+                func(itr+1, xi)
+                xi.pop()
+                nums[itr], nums[i] = nums[i], nums[itr]
+            return
+    
+        func(0,[])
+        
+        return ans
