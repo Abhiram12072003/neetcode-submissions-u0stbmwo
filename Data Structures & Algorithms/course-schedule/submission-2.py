608e3c8593206eed1f27a2b09d7ff2a1684b423f
Abhiram12072003 <94527813+Abhiram12072003@users.noreply.github.com>
2026-04-08 21:48:32 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d = defaultdict(list)
+        vis = [False]*numCourses
+        visited = [False]*numCourses
+
+        for i in range(len(prerequisites)):
+            x, y = prerequisites[i]
+            d[x].append(y)
+        def dfsLoop(idx):
+            visited[idx] = True
+            for i in d[idx]:
+                # print(i, d[idx], idx, vis)
+                if vis[i]:
+                    return True
+                vis[i] = True
+                if dfsLoop(i):
+                    return True
+                vis[i] = False
+            return False
+        # print(d)
+        for i in range(numCourses):
+            if i in d and not visited[i]:
+                vis[i] = True
+                x = dfsLoop(i)
+                if x:
+                    return False
+                vis[i] = False
+        
+        return True
