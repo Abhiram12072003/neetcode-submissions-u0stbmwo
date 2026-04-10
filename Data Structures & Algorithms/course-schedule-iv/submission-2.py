d4c17438f9fe958b39b6bfb36ebaaa3283f86ba8
Abhiram12072003 <94527813+Abhiram12072003@users.noreply.github.com>
2026-04-10 23:07:25 +0530

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = [[] for i in range(numCourses)]
+
+        for x, y in prerequisites:
+            adj[y].append(x)
+        
+        d = defaultdict(set)
+
+        def dfs(x):
+            if x in d:
+                return d[x]
+            
+            for chd in adj[x]:
+                d[x] |= dfs(chd)
+            d[x].add(x)
+            return d[x]
+        
+        for i in range(numCourses):
+            dfs(i)
+        
+        vis = []
+        for x, y in queries:
+            if x in d[y]:
+                vis.append(True)
+            else:
+                vis.append(False)
+        return vis
